dd53f08bb2c4f62ff1067f44740889631f987db4
Suraj <suraj02@gmail.com>
2013-04-13 08:14:58 +0530

    Version1 Bug Fixed

--- contact.php
+++ contact.php
@@ -5,10 +5,10 @@
 	Author			:		Steve Surya
 	Date			:		Apr 13,2013
 	Purpose			:		This file would ionitialize the site
-	Version			:		1.0
+	Version			:		1.1
 */	
 
 $contact = "contact oerson goes here";
-
+//fixing version1 bug
 	
 ?>
